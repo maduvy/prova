3c222733bbaee71106d3284e321cdfdf4e65060f
maduvy <143403284+maduvy@users.noreply.github.com>
2023-10-30 08:13:39 -0300

    prova:)
    #dias_de_luta

---
+++ prova.py
@@ -0,0 +1,6 @@
+num=int(input('digite um número'))
+if num>=0:
+    print('o numero {} que vc digitou é posito '.format(num))
+else:
+    num<=0;
+    print('o numero {} que vc digitou é negativo '.format(num))
